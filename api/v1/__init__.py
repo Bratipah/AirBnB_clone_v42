fa12e049208c435c066d5d45173fd3afc05e7d1b
Bratipah <bratipahmh@gmail.com>
2023-01-30 14:16:08 -0800

    copy airbnbclone3

---
+++ api/v1/__init__.py
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+#!/usr/bin/python3
+"""Initializing a blueprint"""
+from flask import Blueprint
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+=======
+>>>>>>> c70704c412cb7e97666a66af7099d981dd121065
